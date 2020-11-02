fdfb751e1e7ef783479541f63256cbb4d35d2aa9
armatin7 <armatin7@gmail.com>
2020-11-02 11:37:30 -0800

    fixed map struct and lombok

--- dto/src/main/java/com/armatin/dto/UserDto/UserMapper.java
+++ dto/src/main/java/com/armatin/dto/UserDto/UserMapper.java
@@ -3,13 +3,23 @@ package com.armatin.dto.UserDto;
 import com.armatin.model.User;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mappings({
+            @Mapping(source = "username", target = "username"),
+            @Mapping(source = "password", target = "password")
+    })
     UserDto toDto(User user);
 
+    @Mappings({
+                    @Mapping(source = "username", target = "username"),
+                    @Mapping(source = "password", target = "password")
+            })
     User toEntity(UserDto userDto);
 }
